0a3fe19db0248195f22a2bac2ae6ae3bd626a619
Sooyoung Yoon <ayoooo240@gmail.com>
2022-08-06 17:20:33 +0900

    🔵 오늘 날짜에 파란 동그라미 표시

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,13 +1,22 @@
+import dayjs from 'dayjs';
 import React from 'react';
 
 const Day = ({ day, rowIdx }) => {
+  const getCurrentDayClass = () => {
+    return day.format('DD-MM-YY') === dayjs().format('DD-MM-YY') // 오늘 날짜면!
+      ? 'bg-blue-600 text-white rounded-full w-7' // 파란 동그라미를 일에 표시
+      : '';
+  };
+
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
         {rowIdx === 0 && (
           <p className="text-sm mt-1">{day.format('ddd').toUpperCase()}</p>
         )}
-        <p className="text-sm p-1 my-1 text-center">{day.format('DD')}</p>
+        <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>
+          {day.format('DD')}
+        </p>
       </header>
     </div>
   );

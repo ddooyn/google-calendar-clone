e057312d6de1d593aa15c28b655bea440f30583c
Sooyoung Yoon <ayoooo240@gmail.com>
2022-08-09 11:04:50 +0900

    사이드바 스몰 캘린더 생성

---
+++ src/components/SmallCalendar.js
@@ -0,0 +1,48 @@
+import React, { useContext, useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import { getMonth } from '../util';
+import GlobalContext from '../context/GlobalContext';
+
+const SmallCalendar = () => {
+  const [currentMonthIdx, setCurrentMonthIdx] = useState(dayjs().month());
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  useEffect(() => {
+    setCurrentMonth(getMonth(currentMonthIdx));
+  }, [currentMonthIdx]);
+
+  const { monthIndex } = useContext(GlobalContext);
+
+  useEffect(() => {
+    setCurrentMonthIdx(monthIndex);
+  }, [monthIndex]);
+
+  const handlePrevMonth = () => {
+    setCurrentMonthIdx((currentMonthIdx) => currentMonthIdx - 1);
+  };
+
+  const handleNextMonth = () => {
+    setCurrentMonthIdx((currentMonthIdx) => currentMonthIdx + 1);
+  };
+
+  return (
+    <div className="mt-9">
+      <header className="flex justify-between">
+        <p className="text-gray-500 font-bold">
+          {dayjs(new Date(dayjs().year(), currentMonthIdx)).format('MMMM YYYY')}
+        </p>
+        <button onClick={handlePrevMonth}>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_left
+          </span>
+        </button>
+        <button onClick={handleNextMonth}>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_right
+          </span>
+        </button>
+      </header>
+    </div>
+  );
+};
+
+export default SmallCalendar;
